365b864a4e663f0acdb161ded9cd3e2bab232616
saikeiLEe <790006739@qq.com>
2019-10-08 11:47:51 +0800

    忘记提交包

---
+++ src/main/java/com/njs/agriculture/mapper/ProductActivationMapper.java
@@ -0,0 +1,23 @@
+package com.njs.agriculture.mapper;
+
+import com.njs.agriculture.pojo.ProductActivation;
+
+import java.util.List;
+
+public interface ProductActivationMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(ProductActivation record);
+
+    int insertSelective(ProductActivation record);
+
+    ProductActivation selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(ProductActivation record);
+
+    int updateByPrimaryKey(ProductActivation record);
+
+    List<ProductActivation> getByUserId(int userId);
+
+    int checkByCode(String  code);
+}
---
+++ src/main/java/com/njs/agriculture/pojo/ProductActivation.java
@@ -0,0 +1,78 @@
+package com.njs.agriculture.pojo;
+
+import java.util.Date;
+
+public class ProductActivation {
+    private Integer id;
+
+    private String code;
+
+    private Integer batchId;
+
+    private Integer userId;
+
+    private Date createTime;
+
+    private String productName;
+
+    public ProductActivation(Integer id, String code, Integer batchId, Integer userId, Date createTime, String productName) {
+        this.id = id;
+        this.code = code;
+        this.batchId = batchId;
+        this.userId = userId;
+        this.createTime = createTime;
+        this.productName = productName;
+    }
+
+    public ProductActivation() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public Integer getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(Integer batchId) {
+        this.batchId = batchId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName == null ? null : productName.trim();
+    }
+}
