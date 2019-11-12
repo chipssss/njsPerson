931fad2c3f976028b2f3edede8721be1b957d19f
chipssss <Bruslin572>
2019-11-12 10:53:02 +0800

    更改授权码为"zijin"

--- src/main/java/com/njs/agriculture/common/Const.java
+++ src/main/java/com/njs/agriculture/common/Const.java
@@ -11,7 +11,7 @@ import java.util.Set;
  */
 public class Const {
 
-    public static final String AUTH_CODE = "zijing";
+    public static final String AUTH_CODE = "zijin";
 
     public static final String CURRENT_ADMIN = "currentAdmin";
 
